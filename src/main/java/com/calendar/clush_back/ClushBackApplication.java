1afc7e74ea7086d19924544429f07364dd4a971b
[Cloud3] 우문식 <woomoon1107@gmail.com>
2024-09-04 11:53:52 +0900

    Add: 스타터 생성

---
+++ src/main/java/com/calendar/clush_back/ClushBackApplication.java
@@ -0,0 +1,13 @@
+package com.calendar.clush_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClushBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClushBackApplication.class, args);
+	}
+
+}
